9655edf6278fb072e90099574a2b67b09804dfe6
FlyingGraysons <hmwildermuth@gmail.com>
2015-10-27 23:07:47 -0400

    update
    needs work

--- crack.d
+++ crack.d
@@ -6,12 +6,31 @@ only runs on Unix systems cos it uses terminal commands
 */
 import std.stdio;
 
-void main() {
-  string usr;
+void main(string args[]) {
+  if (args.length >= 1) {
+    check();
+  }
+  else {
+    help();
+  }
+
+/*  string usr;
   writeln("Please enter the name of the user who's password you would like to crack: ");
 //doesn't work, it needs chomp instead of the usual formatter
   readf(" %s", &usr);
   writeln(&usr);
+
+  thats poopy
+  */
+}
+
+void help() {
+  writeln("Please enter the argument '-u' then a username")
+}
+
+void check() {
+  if ((args[1] = "-u") && (args[2]!== ""))
+    
 }
 
 /*void crack(string word) {
